89327a6ef8328e3741697199d84e31d05693754b
love2hina <webmaster@love2hina.net>
2022-12-25 00:25:45 +0900

    分離

---
+++ MauiApp1/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiApp1.Views;
+
+public partial class AboutPage : ContentPage
+{
+    public AboutPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+
+}
