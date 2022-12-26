df6fba4e475fe8095e895eea557cdc76bba502c8
love2hina <webmaster@love2hina.net>
2022-12-26 22:44:19 +0900

    foldableのテスト

--- MauiApp1/MauiProgram.cs
+++ MauiApp1/MauiProgram.cs
@@ -1,18 +1,21 @@
 ﻿namespace love2hina.Windows.MAUI.PhotoViewer;
 
+using Microsoft.Maui.Foldable;
+
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+        builder.UseFoldable();
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
---
+++ MauiApp1/Pages/DirectoryView.xaml.cs
@@ -0,0 +1,9 @@
+namespace love2hina.Windows.MAUI.PhotoViewer.Pages;
+
+public partial class DirectoryView : ContentPage
+{
+	public DirectoryView()
+	{
+		InitializeComponent();
+	}
+}
