65b19b1ce627cb8a8ecf60c5bb94708917d5b22a
love2hina <webmaster@love2hina.net>
2022-12-30 10:31:47 +0900

    ナビゲーション対応

--- MauiApp1/Controls/DirectoryView.xaml.cs
+++ MauiApp1/Controls/DirectoryView.xaml.cs
@@ -1,9 +1,40 @@
-﻿namespace love2hina.Windows.MAUI.PhotoViewer.Controls;
+﻿using System.Collections.Generic;
+
+namespace love2hina.Windows.MAUI.PhotoViewer.Controls;
 
 public partial class DirectoryView : CollectionView
 {
-	public DirectoryView()
-	{
-		InitializeComponent();
-	}
+
+    public DirectoryView()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly BindableProperty TargetDirectoryProperty =
+        BindableProperty.Create("TargetDirectory", typeof(DirectoryInfo), typeof(DirectoryView), null);
+    public DirectoryInfo TargetDirectory
+    {
+        get => (DirectoryInfo)GetValue(TargetDirectoryProperty);
+        set => SetValue(TargetDirectoryProperty, value);
+    }
+
+    public IEnumerable<DirectoryInfo> Directories
+    {
+        get => TargetDirectory?.EnumerateDirectories() ??
+            (from d in DriveInfo.GetDrives()
+             where d.IsReady
+             select d.RootDirectory);
+    }
+
+    async void CollectionView_SelectionChanged(System.Object sender, Microsoft.Maui.Controls.SelectionChangedEventArgs e)
+    {
+        DirectoryInfo dir = e.CurrentSelection.FirstOrDefault() as DirectoryInfo;
+        var navigationParameter = new Dictionary<string, object>
+        {
+            { "TargetDirectory", dir }
+        };
+        Console.WriteLine($"DirectoryView changed: {dir}");
+        await Shell.Current.GoToAsync("//Test", false, navigationParameter);
+    }
+
 }
--- MauiApp1/Pages/DirectoryPage.xaml.cs
+++ MauiApp1/Pages/DirectoryPage.xaml.cs
@@ -1,9 +1,28 @@
 namespace love2hina.Windows.MAUI.PhotoViewer.Pages;
 
+[QueryProperty(nameof(TargetDirectory), "TargetDirectory")]
 public partial class DirectoryPage : ContentPage
 {
-	public DirectoryPage()
-	{
-		InitializeComponent();
-	}
+
+    private DirectoryInfo m_dirTarget;
+
+    public DirectoryPage()
+    {
+        InitializeComponent();
+    }
+
+    public DirectoryInfo TargetDirectory
+    {
+        get => m_dirTarget;
+        set
+        {
+            Console.WriteLine($"DirectoryPage changed: {value}");
+            if (m_dirTarget != value)
+            {
+                m_dirTarget = value;
+                OnPropertyChanged(nameof(TargetDirectory));
+            }
+        }
+    }
+
 }
