d3a64dab43242013bf5bd64f632847c7e8e99101
love2hina <webmaster@love2hina.net>
2023-01-11 23:19:35 +0900

    テストプロジェクトの追加

---
+++ TestProject/Files/ThumbnailLoaderTest.cs
@@ -0,0 +1,18 @@
+﻿using love2hina.Windows.MAUI.PhotoViewer.Common.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace love2hina.Windows.MAUI.PhotoViewer.Test.Files;
+
+public class ThumbnailLoaderTest : Initializer
+{
+
+    [Fact]
+    public void Test()
+    {
+        using (var context = FirebirdContextFactory.Create())
+        {
+            context.Database.Migrate();
+        }
+    }
+
+}
---
+++ TestProject/Initializer.cs
@@ -0,0 +1,13 @@
+﻿using love2hina.Windows.MAUI.PhotoViewer.Common.Database;
+
+namespace love2hina.Windows.MAUI.PhotoViewer.Test;
+
+public class Initializer
+{
+
+    static Initializer()
+    {
+        FirebirdContextFactory.Initialize(Environment.CurrentDirectory);
+    }
+
+}
