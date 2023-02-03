432a0808c12da5dbade3a90478ebbeb095152054
love2hina <webmaster@love2hina.net>
2023-02-03 20:36:08 +0900

    Resizeのバグ回避

--- CommonLibrary/Files/ThumbnailLoader.cs
+++ CommonLibrary/Files/ThumbnailLoader.cs
@@ -3,6 +3,7 @@ using love2hina.Windows.MAUI.PhotoViewer.Common.Database.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Maui.Graphics;
 using Microsoft.Maui.Graphics.Skia;
+using SkiaSharp;
 
 namespace love2hina.Windows.MAUI.PhotoViewer.Common.Files;
 
@@ -44,18 +45,25 @@ public static class ThumbnailLoader
         using (var stream = file.OpenRead())
         {
             image = SkiaImage.FromStream(stream);
-                // TODO: リサイズすると吹っ飛ぶ
-                //?.Resize(256.0f, 256.0f, ResizeMode.Bleed, true);
         }
 
         if (image != null)
         {
-            cache = new ThumbnailCache(file)
+            // 縮小する
+            var rate = Math.Min(Math.Min(256.0f / image.Width, 256.0f / image.Height), 1.0f);
+            var size = new SizeF(image.Width * rate, image.Height * rate);
+
+            using (var context = new SkiaBitmapExportContext((int)size.Width, (int)size.Height, 1))
             {
-                Width = (int)image.Width,
-                Height = (int)image.Height,
-                PngData = image.AsBytes(ImageFormat.Png)
-            };
+                context.Canvas.DrawImage(image, 0, 0, size.Width, size.Height);
+
+                cache = new ThumbnailCache(file)
+                {
+                    Width = (int)size.Width,
+                    Height = (int)size.Height,
+                    PngData = context.AsBytes(ImageFormat.Png)
+                };
+            }
 
             using (var context = FirebirdContextFactory.Create())
             {
@@ -65,6 +73,29 @@ public static class ThumbnailLoader
         }
 
         return cache;
+    }
+
+    internal static byte[]? AsBytes(this SkiaBitmapExportContext target, ImageFormat format = ImageFormat.Png, float quality = 1.0f)
+    {
+        if (target == null) return null;
+
+        int skQuality = (int)(100.0f * quality);
+        SKEncodedImageFormat skFormat = format switch
+        {
+            ImageFormat.Png => SKEncodedImageFormat.Png,
+            ImageFormat.Jpeg => SKEncodedImageFormat.Jpeg,
+            ImageFormat.Bmp or ImageFormat.Gif or ImageFormat.Tiff => throw new PlatformNotSupportedException($"Skia does not support {format} format."),
+            _ => throw new ArgumentOutOfRangeException(nameof(format), format, null)
+        };
+
+        using (var stream = new MemoryStream())
+        {
+            using (var data = target.SKImage.Encode(skFormat, skQuality))
+            {
+                data.SaveTo(stream);
+                return stream.ToArray();
+            }
+        }
     }
 
 }
