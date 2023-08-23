540bbdeaedfdc5fdd5329353c4b4fba57b26e1cb
love2hina <webmaster@love2hina.net>
2023-08-23 18:27:33 +0900

    移動処理(暫定)

--- PhotoLibrary/Pages/ItemViewerPage.xaml.cs
+++ PhotoLibrary/Pages/ItemViewerPage.xaml.cs
@@ -5,6 +5,8 @@ namespace love2hina.Windows.MAUI.PhotoLibrary.Pages;
 public partial class ItemViewerPage : ContentPage, IQueryAttributable
 {
 
+    private int _selectedIndex = -1;
+
     public ItemViewerPage()
     {
         FileEntries = Utils.EmptyFileEntryList;
@@ -12,31 +14,47 @@ public partial class ItemViewerPage : ContentPage, IQueryAttributable
         InitializeComponent();
     }
 
-    // TODO: 微妙
     public void ApplyQueryAttributes(IDictionary<string, object> query)
     {
         FileEntries = (IList<FileEntryCache>)query["FileEntries"];
         SelectedIndex = (int)query["SelectedIndex"];
-        TargetFile_Changed();
     }
 
     public IList<FileEntryCache> FileEntries { get; set; }
 
-    public int SelectedIndex { get; set; }
-
-    protected void TargetFile_Changed()
+    public int SelectedIndex
     {
-        if (0 <= SelectedIndex && SelectedIndex < FileEntries.Count)
+        get => _selectedIndex;
+        set
         {
-            TargetFile = FileEntries[SelectedIndex].Path;
-            OnPropertyChanged(nameof(TargetFile));
+            if (_selectedIndex == value)
+            {
+                // 何もしない
+            }
+            else if (value < 0)
+            {
+                _selectedIndex = value;
+                TargetFile = null;
+                OnPropertyChanged(nameof(SelectedIndex));
+                OnPropertyChanged(nameof(TargetFile));
+            }
+            else if (0 <= value && value < FileEntries.Count)
+            {
+                _selectedIndex = value;
+                TargetFile = FileEntries[SelectedIndex].Path;
+                OnPropertyChanged(nameof(SelectedIndex));
+                OnPropertyChanged(nameof(TargetFile));
+            }
         }
     }
- 
+
     public FileInfo? TargetFile
     {
         get;
         protected set;
     }
+
+    private void BackButton_Clicked(object sender, EventArgs e) => SelectedIndex -= 1;
+    private void NextButton_Clicked(object sender, EventArgs e) => SelectedIndex += 1;
 
 }
